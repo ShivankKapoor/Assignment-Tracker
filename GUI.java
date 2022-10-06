3cd16888c64e1b7adb72e592480f85cee2887104
Shivank Kapoor <47286913+ShivankKapoor@users.noreply.github.com>
2022-10-05 19:19:37 -0500

    Sep GUI File

---
+++ GUI.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class GUI extends JFrame {
+    public GUI(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(1000,600);
+        this.setVisible(true);
+    }
+    
+}
