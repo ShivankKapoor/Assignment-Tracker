113484e3aa5a1a86356aa2e8d72b8844bdb81ac2
Shivank Kapoor <47286913+ShivankKapoor@users.noreply.github.com>
2022-10-05 19:32:59 -0500

    Added Buttons

--- GUI.java
+++ GUI.java
@@ -1,9 +1,26 @@
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class GUI extends JFrame {
     public GUI(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
         this.setSize(1000,600);
+
+        JButton addButton = new JButton("Add");
+        JButton removeButton = new JButton("Remove");
+        JButton editButton = new JButton("Edit");
+
+        this.setLayout(null);
+
+        addButton.setBounds(40, 100, 100, 60);
+        removeButton.setBounds(40, 200, 100, 60);
+        editButton.setBounds(40,300,100,60);
+
+        this.add(addButton);
+        this.add(removeButton);
+        this.add(editButton);
+
         this.setVisible(true);
     }
     
