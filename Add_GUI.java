72f8639c261452fecec4dc299fdc3c3685b58500
Shivank Kapoor <47286913+ShivankKapoor@users.noreply.github.com>
2022-10-15 17:32:13 -0500

    Added the add button on the add gui

--- Add_GUI.java
+++ Add_GUI.java
@@ -5,6 +5,7 @@ public class Add_GUI extends JFrame {
 
     public Add_GUI() {
         String[] typeOfAssignments = { "Homework", "Quiz", "Test" };
+        Integer[] assignmentDifficutly = { 1, 2, 3, 4, 5 };
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         this.setResizable(false);
         this.setSize(500, 700);
@@ -26,6 +27,11 @@ public class Add_GUI extends JFrame {
         JTextField yearField = new JTextField("YYYY");
         JLabel dateLabel = new JLabel("Due Date:");
 
+        JComboBox<Integer> asgnmtDiffBox = new JComboBox<>(assignmentDifficutly);
+        JLabel difLabel = new JLabel("Assignment Diff");
+
+        JButton addAssignment = new JButton("Add");
+
         nameTxtField.setBounds(225, 112, 165, 25);
         nameLabel.setBounds(100, 100, 150, 50);
 
@@ -35,12 +41,19 @@ public class Add_GUI extends JFrame {
         courseNameField.setBounds(225, 212, 165, 25);
         courseNameLabel.setBounds(100, 202, 150, 50);
 
-        monthField.setBounds(225,242,50,25);
-        dayField.setBounds(275,242,50,25);
-        yearField.setBounds(325,242,50,25);
-        dateLabel.setBounds(100,230,150,50);
-        
-        
+        monthField.setBounds(225, 252, 50, 25);
+        dayField.setBounds(275, 252, 50, 25);
+        yearField.setBounds(325, 252, 50, 25);
+        dateLabel.setBounds(100, 240, 150, 50);
+
+        asgnmtDiffBox.setBounds(225,292,75,30);
+        difLabel.setBounds(100, 280, 150, 50);
+
+        addAssignment.setBounds(200,575,100,50);
+
+        this.add(addAssignment);
+        this.add(difLabel);
+        this.add(asgnmtDiffBox);
         this.add(yearField);
         this.add(dateLabel);
         this.add(monthField);
