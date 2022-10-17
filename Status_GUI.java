4abe0184c7c9482ead110b7cf74e1e8b17b63654
Shivank Kapoor <47286913+ShivankKapoor@users.noreply.github.com>
2022-10-16 21:13:10 -0500

    Update Status_GUI.java

--- Status_GUI.java
+++ Status_GUI.java
@@ -10,12 +10,20 @@ public class Status_GUI extends JFrame  {
         this.setResizable(false);
         this.setSize(500, 250);
         this.setTitle("Toggle Status");
+        this.setLayout(null);
 
         JTextField IDInput = new JTextField();
-        JLabel IDLabel = new JLabel("ID: ");
+        JLabel IDLabel = new JLabel("ID:");
 
         JButton IDSubmit = new JButton("Submit");
 
+        IDInput.setBounds(240,80,100,40);
+        IDLabel.setBounds(180,70,40,60);
+        IDSubmit.setBounds(200, 160, 100, 40);
+
+        this.add(IDInput);
+        this.add(IDLabel);
+        this.add(IDSubmit);
         this.setVisible(true);
     }
     
