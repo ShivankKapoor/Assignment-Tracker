a9e7d9a0b589fd28a2a123ef4f4ac29ed058ab19
Shivank Kapoor <47286913+ShivankKapoor@users.noreply.github.com>
2022-10-16 21:17:06 -0500

    Update Remove_GUI.java

--- Remove_GUI.java
+++ Remove_GUI.java
@@ -1,4 +1,4 @@
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class Remove_GUI extends JFrame{
     public Remove_GUI(){
@@ -6,7 +6,20 @@ public class Remove_GUI extends JFrame{
         this.setResizable(false);
         this.setSize(500, 250);
         this.setTitle("Remove Class");
+        this.setLayout(null);
 
+        JTextField IDInput = new JTextField();
+        JLabel IDLabel = new JLabel("ID:");
+
+        JButton IDSubmit = new JButton("Submit");
+
+        IDInput.setBounds(240,80,100,40);
+        IDLabel.setBounds(180,70,40,60);
+        IDSubmit.setBounds(200, 160, 100, 40);
+
+        this.add(IDInput);
+        this.add(IDLabel);
+        this.add(IDSubmit);
         this.setVisible(true);
     }
     
